7ab549fe7ed61e6d9c481b4a5d48603340233f93
apple <apple@apples-MacBook-Pro.local>
2023-12-30 23:55:39 +0530

    Setup app and fixed port value

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const app = express()
 const geoCode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+
+const port = process.env.PORT || 3000
+
 //Define Paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -92,6 +95,6 @@ app.get('*', (req, res)=>{
     })
 })
 
-app.listen(3000, ()=>{
-    console.log('Server is up on port 3000')
+app.listen(port, ()=>{
+    console.log('Server is up on port', port)
 })
