7bc18ae5f3b06eab0beb440736eab0d4a3842ce9
apple <apple@apples-MacBook-Pro.local>
2024-01-07 00:47:41 +0530

    Added new functionality of locate me

--- public/JS/app.js
+++ public/JS/app.js
@@ -2,8 +2,55 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+const messageThree = document.querySelector('#message-3')
+const userInput = document.getElementById('userInp');
+const locateMeButton = document.getElementById('locateMeBtn');
 
-weatherForm.addEventListener('submit', (e)=>{
+locateMeButton.addEventListener('click', ((e) => {
+    e.preventDefault()
+    function getUserLocation() {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                if (position.coords) {
+                    const latitude = position.coords.latitude;
+                    const longitude = position.coords.longitude;
+                    getReverseGeocodingData(latitude, longitude);
+                }
+            },
+            error => {
+                console.error('Error getting user location:', error.message);
+                messageThree.textContent = 'Error getting user location';
+            }
+        );
+    }
+    getUserLocation();
+            function getReverseGeocodingData(lat, lon) {
+                const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`
+                fetch(url).then(response => response.json()).then(data => {
+                        if (data.address.city) {
+                            const address = data.address.city
+                            fetch(`/weather?address=${address}`).then((response) => {
+                                response.json().then((data) => {
+                                    if (data.error) {
+                                        messageOne.textContent = data.error
+                                    }
+                                    messageOne.textContent = data.location
+                                    messageTwo.textContent = data.forecastData
+                                })
+                            })
+                        } else {
+                            console.error('Reverse Geocoding failed.');
+                            messageThree.textContent = 'Error retrieving location';
+                        }
+                    })
+                    .catch(error => {
+                        console.error('Error during reverse geocoding:', error);
+                        messageThree.textContent = 'Error retrieving location';
+                    });
+            }
+}))
+
+weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
@@ -11,13 +58,13 @@ weatherForm.addEventListener('submit', (e)=>{
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch(`/weather?address=${location}`).then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-            messageOne.textContent = data.error
-        }
-        messageOne.textContent = data.location
-        messageTwo.textContent = data.forecastData
+    fetch(`/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            }
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecastData
+        })
     })
-})
 })
